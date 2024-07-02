a8d1e5e6b0f21102363c8fbb4c838d3458d2677a
Jerome <jerome.gir@gmail.com>
2024-07-03 01:48:04 +0200

    tablet version

--- app/components/crew/BackgroundCrew.tsx
+++ app/components/crew/BackgroundCrew.tsx
@@ -4,8 +4,20 @@ export default function BackgroundCrew() {
   return (
     <>
       <Image
-        className="-z-10"
+        className="block sm:hidden -z-10"
         src="/assets/crew/background-crew-mobile.jpg"
+        alt="homepage background"
+        fill
+      ></Image>
+      <Image
+        className="hidden sm:block -z-10"
+        src="/assets/crew/background-crew-tablet.jpg"
+        alt="homepage background"
+        fill
+      ></Image>
+      <Image
+        className="hidden lg:block -z-10"
+        src="/assets/crew/background-crew-desktop.jpg"
         alt="homepage background"
         fill
       ></Image>
--- app/components/crew/MainContentCrew.tsx
+++ app/components/crew/MainContentCrew.tsx
@@ -34,18 +34,18 @@ export default function MainContentCrew({
 }: MainContentCrewProps) {
   return (
     <div className="flex flex-col items-center p-4">
-      <div className="w-[327px] h-[744px] sm:mt-10">
-        <h1 className="font-barlow_condensed uppercase text-center text-white tracking-widest mb-16 lg:text-2xl">
-          <span className="font-bold mr-6 tracking-widest lg:text-3xl">02</span>{" "}
-          meet your crew
-        </h1>
-        <h2 className="font-bellefair text-lg text-center text-white uppercase font-thin">
+      <h1 className="font-barlow_condensed uppercase text-center text-white  tracking-widest mb-16 sm:mb-0 sm:text-xl sm:p-6 sm:text-left lg:text-2xl  w-screen">
+        <span className="font-bold mr-6 tracking-widest lg:text-3xl">02</span>{" "}
+        meet your crew
+      </h1>
+      <div className="w-[327px] sm:w-screen sm:mt-10">
+        <h2 className="font-bellefair text-lg text-center text-white uppercase font-thin sm:text-2xl sm:mb-4 ">
           {title}
         </h2>
-        <h3 className="font-bellefair text-center text-2xl mt-2 mb-6 text-white uppercase">
+        <h3 className="font-bellefair text-center text-2xl sm:text-5xl mt-2 mb-6 text-white uppercase">
           {name}
         </h3>
-        <p className="text-light-blue font-extralight text-center">
+        <p className="text-light-blue font-extralight text-center sm:px-48 sm:text-lg">
           {description}
         </p>
         <nav className="mt-16">
@@ -66,7 +66,7 @@ export default function MainContentCrew({
         </nav>
         <div className="flex items-center justify-center mt-12">
           <Image
-            className=""
+            className="w-[271px] sm:w-[433px]"
             src={imageSrc}
             alt={imageAlt}
             width={271}
